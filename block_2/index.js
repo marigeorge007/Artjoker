de1d15604f14eeb8dc7e7fdf58f1c773f993641c
marigeorge007 <maryofgeorge@gmail.com>
2021-11-25 17:10:56 -0500

    fixid bugs

--- block_2/index.js
+++ block_2/index.js
@@ -1,49 +1,15 @@
 //1 Написать свою реализацию функций bind, call. Новая реализация должна по функционалу работать аналогично как и соответствующие стандартные функции. Без использования стандартных функций.
-// my bind
-const person = {
-  name: 'Mark',
-}
-
-function info(phone, email) {
-  console.log(`Имя: ${this.name}, Теl.:${phone}, Email: ${email}`);
-};
-
-function myBind(fn, context, ...rest) {
-  return function (...args) {
-    let uniqId = Date.now().toString();;
-    context[uniqId] = fn;
-    let result = context[uniqId](...rest.concat(args));
-    delete context[uniqId];
-    return result;
-  }
-}
-
-myBind(info, person)('12345', 'm@gmail.com');
-myBind(info, person, '12345')('m@gmail.com');
-myBind(info, person, '12345', 'm@gmail.com')();
-
 //my bind
 
 Function.prototype.myBind = function (object, ...rest) {
+  object[symbol] = this;
   return function (...args) {
     let symbol = Symbol();
-    object[symbol] = this;
     let result = object[symbol](...rest, ...args);
     delete object[symbol];
     return result;
-  }
+  };
 }
-
-
-// Call
-function myCall(fn, context, ...args) {
-  let uniqId = Date.now().toString();
-  context[uniqId] = fn;
-  let result = context[uniqId](...args);
-  delete context[uniqId];
-  return result;
-};
-//myCall(info, person, '1234', 'c@gmail.com');
 
 // Call 
 Function.prototype.myCall = function (object, ...args) {
@@ -69,7 +35,7 @@ Array.prototype.myFilter = function (callback) {
   let arr = [];
 
   for (let i = 0; i < this.length; i++) {
-    if (callback(this[i]) === true) {
+    if (callback(this[i])) {
       arr.push(this[i]);
     }
   }
@@ -78,11 +44,11 @@ Array.prototype.myFilter = function (callback) {
 
 //Reduce
 Array.prototype.myReduce = function (callback) {
-  let res = 0;
-  for (let i = 0; i < this.length; i++) {
-    callback(res = res + this[i]);
+  let acc = 0; // шаг1 декларируем аккумулятор;
+  for (let i = 0; i < this.length; i++) { // шаг2 выполняется цикл;
+    callback(acc = acc + this[i]); //  шаг3 складываем сумму всех элементов в массиве в aккумулятор шаг1;
   }
-  return res;
+  return acc; //  шаг4 возвращаем аккумулятор;
 }
 
 //forEach
@@ -106,12 +72,6 @@ let fibonacciIterator = {
         };
       }
     };
-  }
-};
-
-for (let num of fibonacciIterator) {
-  if (num > 50) {
-    break;
   }
 };
 
